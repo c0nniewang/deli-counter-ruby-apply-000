ab52d143637ba1c115e60982701a6324013e31f7
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-08 23:20:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(arr)
+  if arr.length == 0
     puts "The line is currently empty."
-  else katz_deli.length > 0
+  else
     str = "The line is currently:"
     newstr = ""
-    katz_deli.each_with_index do |el, i|
+    arr.each_with_index do |el, i|
       newstr += " " + (i + 1).to_s + ". #{el}"
     end
     puts str + newstr
   end
 end
+
+def take_a_number(arr, )
