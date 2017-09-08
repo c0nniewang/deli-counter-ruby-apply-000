f71205470f0e7d4a7735ac7f6b7c1a6587721c7c
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-08 23:18:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 def line(katz_deli)
   if katz_deli.length == 0
-    "The line is currently empty."
+    puts "The line is currently empty."
   else katz_deli.length > 0
     str = "The line is currently: "
     newstr = ""
     katz_deli.each_with_index do |el, i|
       newstr += (i + 1).to_s + ". #{el} "
     end
-    str + newstr
+    puts str + newstr
   end
 end
