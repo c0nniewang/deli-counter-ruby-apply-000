77eaef94be7558b8ffd558b9e49c0b9b3df42548
Connie Wang <conniewang2012@u.northwestern.edu>
2017-09-08 23:26:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(arr, name)
 end
 
 def now_serving(arr)
-  if arr == 0
+  if arr.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{arr[0]}."
